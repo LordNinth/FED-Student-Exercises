310aee332ca458bc361d301eaf79f88f82f63c28
LordNinth <rr621801@gmail.com>
2023-04-25 19:01:56 +0100

    wk 1

---
+++ 3 - JS/Week 1/wk1.js
@@ -0,0 +1,22 @@
+//ex 1
+
+const userPrompt = "hello world!"
+console.log(userPrompt)
+
+//ex 2
+//method 1
+const a = 2
+const b = 2
+const sum = a + b 
+console.log(sum)
+
+//method 2
+function add(a,b) {
+    return a+b
+}
+console.log(add(2,2))
+
+//method 3
+
+const addFunctionES6 = (x,y) => x*y
+console.log(addFunctionES6(3,4))
---
+++ 3 - JS/Week 2/wk2.js
@@ -0,0 +1,53 @@
+//exercise 1
+
+//arrow function
+/*const evenCheck1 = (num) => {
+    if (num%2 === 0){
+        return "Even"
+    }
+    return "odd"
+}
+
+console.log(evenCheck1(0)) */
+
+/* Exercise 2
+
+Create a function that generates a random number between 1 and 10.
+
+>💡 Hint: `prompt()` the user to guess the number and provide feedback if the guess is too high or too low.
+>
+>💡 Hint: break it down into chunks
+>
+>```js
+>if ( number too high ){
+>    // do this
+>}
+>else if ( number too low ){
+>    // do this instead
+>}
+*/
+
+
+
+
+function numGuess(){
+    const x = Math.floor((Math.random() * 10) + 1);
+    let num2 =parseInt(prompt("guess num between 0-10"));
+    while (num2!=x) {
+        num2 =parseInt(prompt("guess num between 0-10"));
+        if (num2 == x)
+        {
+            return "correct"
+        }
+        else if (num2 <x) 
+        {
+    
+            return"x is higher"
+        }
+    }
+
+    
+   
+
+
+console.log(numGuess())
